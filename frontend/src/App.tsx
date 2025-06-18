d57ac59b8d358a4d4fae6563a674f1f16b63ab84
LuigiDotSRC <luigi.terashima@gmail.com>
2025-06-18 09:19:47 -0400

    init frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+
+function App() {
+  return (
+    <div>
+      <h1 className='text-3xl font-bold underline text-amber-400'>Hello World</h1>
+      <Button>Click me</Button>
+    </div>
+  )
+}
+
+export default App
